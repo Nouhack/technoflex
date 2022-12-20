9a7a46fbfa7681b15b4cf01e21b22fd3acabcd08
Nouhack <nouh.saiche@gmail.com>
2022-12-20 05:15:00 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
+
