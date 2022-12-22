ab4eb1cd30df806fdef87d6222bc4e15d4756a9b
Nouhack <nouh.saiche@gmail.com>
2022-12-22 19:59:51 +0100

    correct typo

--- components/Header.jsx
+++ components/Header.jsx
@@ -222,14 +222,14 @@ export default function Nav() {
                       </Link>
                     </li>
                     <li>
-                      <Ling
+                      <Link
                         href="/"
                         class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                         aria-label="Sign up"
                         title="Sign up"
                       >
                         Sign up
-                      </Ling>
+                      </Link>
                     </li>
                   </ul>
                 </nav>
